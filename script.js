59024900ccd2ccf2e9124ec8b5eff529c6cdff99
aliceakesson <alicek732@gmail.com>
2023-06-18 19:34:13 +0200

    Algorithm to create new pipes

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-var pipes = document.querySelectorAll('.pipe');
 const middleSpace = 200;
-const spaceBetweenPipes = 300;
 
-pipes.forEach(pipe => {
-    const value = parseFloat(pipe.getAttribute('height-percentage'));
+function reloadPipes() {
+    var pipes = document.querySelectorAll('.pipe');
+    pipes.forEach(pipe => {
+        const value = parseFloat(pipe.getAttribute('height-percentage'));
+    
+        const topHeight = parseInt(value * pipe.clientHeight) - parseInt(middleSpace/2); 
+        const bottomHeight = parseInt((1 - value) * pipe.clientHeight) - parseInt(middleSpace/2);
+    
+        const top = document.createElement('div');
+        const bottom = document.createElement('div');
+    
+        top.classList.add('pipePart');
+        bottom.classList.add('pipePart');
+    
+        top.style.height = topHeight + "px";
+        bottom.style.height = bottomHeight + "px";
+    
+        top.style.marginBottom = middleSpace + "px";
+    
+        pipe.appendChild(top);
+        pipe.appendChild(bottom);
+    
+    
+    });
+}
 
-    const topHeight = parseInt(value * pipe.clientHeight) - parseInt(middleSpace/2); 
-    const bottomHeight = parseInt((1 - value) * pipe.clientHeight) - parseInt(middleSpace/2);
+function createPipe(value) {
+    const pipe = document.createElement('div');
+    pipe.classList.add('pipe');
+    pipe.setAttribute('height-percentage', value);
 
-    const top = document.createElement('div');
-    const bottom = document.createElement('div');
+    document.getElementById('pipes').appendChild(pipe);
+}
 
-    top.classList.add('pipePart');
-    bottom.classList.add('pipePart');
-
-    top.style.height = topHeight + "px";
-    bottom.style.height = bottomHeight + "px";
-
-    top.style.marginBottom = middleSpace + "px";
-
-    pipe.appendChild(top);
-    pipe.appendChild(bottom);
-
-});
-
+createPipe(0.5);
+reloadPipes();
